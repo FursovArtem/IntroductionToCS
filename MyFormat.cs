1358f1a2c912715afb954e2c359ca63583472de1
Artem <aztec@DESKTOP-CK171NA>
2023-11-12 23:51:58 +0400

    field

---
+++ MyFormat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyFormat
+{
+    public class MyFormat
+    {
+        public static MyFormat()
+        {
+            private static string MyReplacer(string prime_expr, string change_what, string change_into)
+            {
+                prime_expr = prime_expr.Replace(" ", "");
+                int index1 = prime_expr.IndexOf(change_what);
+                int index2 = index1 + change_what.Length;
+                string result = prime_expr;
+                if (index1 != -1)
+                {
+                    result = "";
+                    for (int i = 0; i < index1; i++) result += prime_expr[i];
+                    result += change_into;
+                    for (int i = index2; i < prime_expr.Length; i++) result += prime_expr[i];
+                }
+                return result;
+            }
+            private static string MyReplacer(string prime_expr, int from_where, int to_where, string change_into)
+            {
+                prime_expr = prime_expr.Replace(" ", "");
+                string result = "";
+                for (int i = 0; i < from_where; i++) result += prime_expr[i];
+                result += change_into;
+                for (int i = to_where + 1; i < prime_expr.Length; i++) result += prime_expr[i];
+                return result;
+            }
+        }
+    }
+}
--- Snake/Program.cs
+++ Snake/Program.cs
@@ -7,9 +7,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Schema;
 
-namespace Point
+namespace Snake
 {
-    class Field
+    public class Field
     {
         const char UpLeftCorner = '╔';
         const char DownLeftCorner = '╚';
@@ -23,12 +23,12 @@ namespace Point
         private ConsoleColor borderColor;
         private char[,] field;
 
-        public Field(int width = 25, int height = 100, ConsoleColor borderColor = ConsoleColor.Green)
+        public Field(int width = 100, int height = 25, ConsoleColor borderColor = ConsoleColor.Green)
         {
             this.width = width;
             this.height = height;
             this.borderColor = borderColor;
-            this.field = new char[width, height];
+            this.field = new char[height, width];
         }
         public int Width
         {
@@ -52,37 +52,37 @@ namespace Point
         }
         public void Draw()
         {
-            for (int i = 0; i < width; i++)
+            Console.ForegroundColor = borderColor;
+            for (int i = 0; i < height; i++)
             {
-                for (int j = 0; j < height; j++)
+                for (int j = 0; j < width; j++)
                 {
-                    Console.ForegroundColor = borderColor;
                     if (i == 0 && j == 0) Console.Write(field[i, j] = UpLeftCorner);
-                    else if (i == 0 && j == height - 1) Console.Write(field[i, j] = UpRightCorner);
-                    else if (i == width - 1 && j == 0) Console.Write(field[i, j] = DownLeftCorner);
-                    else if (i == width - 1 && j == height - 1) Console.Write(field[i, j] = DownRightCorner);
-                    else if (i == 0 || i == width - 1) Console.Write(field[i, j] = Horizontal);
-                    else if (j == 0 || j == height - 1) Console.Write(field[i, j] = Vertical);
+                    else if (i == 0 && j == width - 1) Console.Write(field[i, j] = UpRightCorner);
+                    else if (i == height - 1 && j == 0) Console.Write(field[i, j] = DownLeftCorner);
+                    else if (i == height - 1 && j == width - 1) Console.Write(field[i, j] = DownRightCorner);
+                    else if (i == 0 || i == height - 1) Console.Write(field[i, j] = Horizontal);
+                    else if (j == 0 || j == width - 1) Console.Write(field[i, j] = Vertical);
                     else Console.Write(field[i, j] = ' ');
                 }
                 Console.WriteLine();
             }
         }
     };
-    class Point
+    class Player
     {
         private Field field;
         private int x;
         private int y;
-        private char body;
+        private char head;
         private ConsoleColor color;
 
-        public Point(Field field, ConsoleColor color = ConsoleColor.White, int x = 1, int y = 1, char body = (char)164)
+        public Player(Field field, ConsoleColor color = ConsoleColor.White, int y = 1, int x = 1, char head = (char)164)
         {
             this.field = field;
             this.x = x;
             this.y = y;
-            this.body = body;
+            this.head = head;
             this.color = color;
         }
         public int X
@@ -95,10 +95,10 @@ namespace Point
             get { return y; }
             set { y = value; }
         }
-        public char Body
+        public char Head
         {
-            get { return body; }
-            set { body = value; }
+            get { return head; }
+            set { head = value; }
         }
         public ConsoleColor Color
         {
@@ -112,7 +112,7 @@ namespace Point
             Console.ForegroundColor = color;
             Console.CursorTop = x;
             Console.CursorLeft = y;
-            Console.Write(field.AField[x, y] = body);
+            Console.Write(field.AField[x, y] = head);
         }
         public void Clear()
         {
@@ -124,8 +124,8 @@ namespace Point
         {
             Random rand = new Random();
             Clear();
-            x = rand.Next(1, field.Width - 1);
-            y = rand.Next(1, field.Height - 1);
+            x = rand.Next(1, field.Height - 1);
+            y = rand.Next(1, field.Width - 1);
             Set();
         }
         public void Move()
@@ -150,7 +150,7 @@ namespace Point
                     case ConsoleKey.DownArrow:
                     case ConsoleKey.S:
                         {
-                            if (x < field.Width - 2)
+                            if (x < field.Height - 2)
                             {
                                 Clear();
                                 x++;
@@ -172,7 +172,7 @@ namespace Point
                     case ConsoleKey.RightArrow:
                     case ConsoleKey.D:
                         {
-                            if (y < field.Height - 2)
+                            if (y < field.Width - 2)
                             {
                                 Clear();
                                 y++;
@@ -192,9 +192,9 @@ namespace Point
         static void Main(string[] args)
         {
             Random rand = new Random();
-            int vertical = 25, horizontal = 100;
-            Field field = new Field(vertical, horizontal, ConsoleColor.DarkGreen);
-            Point player = new Point(field, ConsoleColor.DarkCyan, rand.Next(1, vertical - 1), rand.Next(1, horizontal - 1));
+            int horizontal = 100, vertical = 25;
+            Field field = new Field(horizontal, vertical, ConsoleColor.Yellow);
+            Player player = new Player(field, ConsoleColor.DarkCyan, rand.Next(1, horizontal - 1), rand.Next(1, vertical - 1));
             field.Draw();
             player.Set();
             player.Move();
